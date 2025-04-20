6e631194fe0e8b8362601c0af716b50222daf8df
Eugene Chekerdes <tchekerdes.vgen@ukr.net>
2025-04-20 18:34:36 +0300

    add filter store

--- src/features/tracks/index.ts
+++ src/features/tracks/index.ts
@@ -1,4 +1,5 @@
 export { TasksFilter } from "./filter-tracks/ui/TasksFilter";
+export { useFilters, useFilterActions } from "./filter-tracks/model/store/fiterStore";
 
 export { SearchInput } from "./search-tracks/ui/SearchInput";
 export { useSearchText, useSearchActions } from "./search-tracks/model/store/searchStore";
