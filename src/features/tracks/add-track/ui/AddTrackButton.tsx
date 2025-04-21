2227a84a06cfa0973b2b4a5f42b53cdc2140e06b
Eugene Chekerdes <tchekerdes.vgen@ukr.net>
2025-04-21 10:53:01 +0300

    refactore add track feat

--- src/features/tracks/add-track/ui/AddTrackButton.tsx
+++ src/features/tracks/add-track/ui/AddTrackButton.tsx
@@ -1,32 +1,24 @@
 import { CirclePlus } from "lucide-react";
-import {
-  Button,
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/shared/ui";
+import { Button } from "@/shared/ui";
 import { AddTrackForm } from "./AddTrackForm";
 import { useState } from "react";
+import { AddTrackDialog } from "./AddTrackDialog";
 
 export const AddTrackButton = () => {
   const [open, setOpen] = useState(false);
   const handleSubmitted = () => setOpen(false);
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>
+    <AddTrackDialog
+      open={open}
+      onOpenChange={setOpen}
+      trigger={
         <Button className="flex items-center gap-x-2 cursor-pointer">
           <CirclePlus />
           <span>Add a track</span>
         </Button>
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Add a track</DialogTitle>
-        </DialogHeader>
-        <AddTrackForm onSubmited={handleSubmitted} />
-      </DialogContent>
-    </Dialog>
+      }
+    >
+      <AddTrackForm onSubmitted={handleSubmitted} />
+    </AddTrackDialog>
   );
 };
