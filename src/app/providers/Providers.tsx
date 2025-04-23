0b3729144bb6c14b06f37d2e1f0d9608f06bf8e3
Eugene Chekerdes <tchekerdes.vgen@ukr.net>
2025-04-24 02:42:47 +0300

    add router

--- src/app/providers/Providers.tsx
+++ src/app/providers/Providers.tsx
@@ -1,7 +1,12 @@
-import { PropsWithChildren } from "react";
+import { RouterProvider } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/shared/configs";
+import { router } from "../routes/router";
 
-export const Providers: React.FC<PropsWithChildren> = ({ children }) => {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+export const Providers = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./app/styles/index.css";
-import { TracksPage, } from "@/pages/tracks";
 import { Providers } from "@/app/providers";
 import { Toaster } from "@/shared/ui";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Providers>
-      <TracksPage />
-    </Providers>
+    <Providers />
     <Toaster />
   </StrictMode>,
 );
